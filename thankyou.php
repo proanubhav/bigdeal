31ef74c55d950e9e0e0c57122b9d89bb2b804e6a
Ashish Gupta <rahulrajedge@gmail.com>
2024-12-03 23:23:41 +0530

    create thankyou page

---
+++ thankyou.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>Contact | BigDeal Supermarket Franchise | Contact Us for Profits</title>
+  <meta name="description"
+    content="Looking to start a Supermarket Franchise? Join BigDeal Supermart and explore a profitable franchise models. Contact us today for more details!">
+  <meta name="keywords"
+    content="Supermarket Franchise, BigDeal Franchise, Growth in Supermarket Franchise,  Supermart franchise in India, Big Deal Supermart, Supermarket Franchise Model">
+
+  <meta property="og:site_name" content="Bigdeal Supermart Franchise">
+  <meta property="og:locale" content="en_US">
+  <meta property="og:title" content="BigDeal Supermarket Franchise | Contact Us for Profits">
+  <meta property="og:url" content="https://www.bigdealsupermart.com/contact">
+  <meta property="og:description"
+    content="Looking to start a Supermarket Franchise? Join BigDeal Supermart and explore a profitable franchise models. Contact us today for more details!">
+  <meta property="og:image" content="https://www.bigdealsupermart.com/assets/img/big-deal-supermart.webp">
+  <meta property="og:image:alt" content="Supermarket Franchise">
+  <meta property="og:type" content="service">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:domain" content="www.bigdealsupermart.com">
+  <!-- <meta name="twitter:site" content="@om_sai_group"> -->
+  <meta name="twitter:title" content="BigDeal Supermarket Franchise | Contact Us for Profits">
+  <meta name="twitter:url" content="https://www.bigdealsupermart.com/contact">
+  <meta name="twitter:description"
+    content="Looking to start a Supermarket Franchise? Join BigDeal Supermart and explore a profitable franchise models. Contact us today for more details!">
+  <meta name="twitter:image" content="https://www.bigdealsupermart.com/assets/img/big-deal-supermart.webp">
+  <meta itemprop="name" content="BigDeal Supermarket Franchise | Contact Us for Profits">
+  <meta itemprop="url" content="https://www.bigdealsupermart.com/contact">
+  <meta itemprop="description"
+    content="Looking to start a Supermarket Franchise? Join BigDeal Supermart and explore a profitable franchise models. Contact us today for more details!">
+  <meta itemprop="image" content="https://www.bigdealsupermart.com/assets/img/big-deal-supermart.webp">
+  <link rel="canonical" href="https://www.bigdealsupermart.com/contact">
+
+  <link rel="icon" href="assets/img/big-deal-favicon.png" sizes="16x16" type="image/png">
+  <meta name="robots" content="index, follow">
+  <meta http-equiv="cache-control" content="max-age=0" />
+  <meta http-equiv="cache-control" content="no-cache" />
+  <meta http-equiv="pragma" content="no-cache" />
+
+  <!-- Stylesheets -->
+  <?php include 'common/stylesheet.php'; ?>
+  <!-- Stylesheets -->
+
+
+</head>
+
+<body>
+  <!-- Header Section HTML -->
+  <?php include 'common/header.php'; ?>
+
+  <section class="thankyou pt-lg-100 pt-md-90 pt-sm-80 pt-xs-70">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-8 offset-lg-2">
+          <div class="text-center mt-3">
+            <div class="thank_image">
+              <img src="assets/img/super_thanks.png">
+            </div>
+            <h1 class="thankTitle">THANK YOU!</h1>
+            <div class="main-content">
+              <i class="fa fa-check main-content__checkmark" id="checkmark"></i>
+              <p>Thanks a bunch for filling that out. It
+                means a lot
+                to us, just like you do! We really appreciate you giving us a moment of your time today. Thanks for
+                being you.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- Footer HTML -->
+  <?php include 'common/footer.php'; ?>
+  <!-- Footer HTML -->
+
+  <!-- Floating Mobile Footer -->
+  <?php include 'common/mobile-footer.php'; ?>
+  <!-- Floating Mobile Footer -->
+
+  <!-- Scripts -->
+  <?php include 'common/scripts.php'; ?>
+  <!-- Scripts -->
+  <script src="assets/js/sheetform.js"></script>
+</body>
+
+</html>
