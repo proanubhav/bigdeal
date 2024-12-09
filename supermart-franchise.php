a556b17686388d02e196725a6f4dcad6b2249669
Anubhav Gupta <proanubhav@gmail.com>
2024-12-09 23:27:01 +0530

    new page added

---
+++ supermart-franchise.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>Start your Franchise with BigDeal Supermart in India</title>
+  <meta name="description"
+    content="Join BigDeal Supermart Franchise for a proven supermart franchise model. Experience profits & growth in supermart franchise with Big Deal. Apply now!">
+  <meta name="keywords"
+    content="Supermarket Franchise, BigDeal Franchise, Growth in Supermarket Franchise,  Supermart franchise in India, Big Deal Supermart, Supermarket Franchise Model">
+
+  <meta property="og:site_name" content="Bigdeal Supermart Franchise">
+  <meta property="og:locale" content="en_US">
+  <meta property="og:title" content="Start your Franchise with BigDeal Supermart in India">
+  <meta property="og:url" content="https://www.bigdealsupermart.com/start-your-franchise">
+  <meta property="og:description"
+    content="Join BigDeal Supermart Franchise for a proven supermart franchise model. Experience profits & growth in supermart franchise with Big Deal. Apply now!">
+  <meta property="og:image" content="https://www.bigdealsupermart.com/assets/img/big-deal-supermart.webp">
+  <meta property="og:image:alt" content="Supermarket Franchise">
+  <meta property="og:type" content="service">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:domain" content="www.bigdealsupermart.com">
+  <!-- <meta name="twitter:site" content="@om_sai_group"> -->
+  <meta name="twitter:title" content="Start your Franchise with BigDeal Supermart in India">
+  <meta name="twitter:url" content="https://www.bigdealsupermart.com/start-your-franchise">
+  <meta name="twitter:description"
+    content="Join BigDeal Supermart Franchise for a proven supermart franchise model. Experience profits & growth in supermart franchise with Big Deal. Apply now!">
+  <meta name="twitter:image" content="https://www.bigdealsupermart.com/assets/img/big-deal-supermart.webp">
+  <meta itemprop="name" content="Start your Franchise with BigDeal Supermart in India">
+  <meta itemprop="url" content="https://www.bigdealsupermart.com/start-your-franchise">
+  <meta itemprop="description"
+    content="Join BigDeal Supermart Franchise for a proven supermart franchise model. Experience profits & growth in supermart franchise with Big Deal. Apply now!">
+  <meta itemprop="image" content="https://www.bigdealsupermart.com/assets/img/big-deal-supermart.webp">
+  <link rel="canonical" href="https://www.bigdealsupermart.com/start-your-franchise">
+
+  <link rel="icon" href="assets/img/big-deal-favicon.png" sizes="16x16" type="image/png">
+  <meta name="robots" content="index, follow">
+  <meta http-equiv="cache-control" content="max-age=0" />
+  <meta http-equiv="cache-control" content="no-cache" />
+  <meta http-equiv="pragma" content="no-cache" />
+
+  <!-- Stylesheets -->
+  <?php include 'common/stylesheet.php'; ?>
+  <!-- Stylesheets -->
+
+
+</head>
+
+<body>
+  <!-- Header Section HTML -->
+  <?php include 'common/header.php'; ?>
+  <!-- Header Section HTML -->
+
+  <article class="service_article mt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="serv_article_card apply_franchise_head">
+            <h1 class="text-center mb-3">Big Profit with BigDeal</h1>
+            <p class="text-center mb-1">Discover a joyous investment opportunity with a Bigdeal Supermarket Franchise!
+              Our franchise model is built for a high-profit margin and strong back up to support you all along the way.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="apply_franchise">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-lg-6 franchise_img">
+            <div class="image d-flex justify-content-center align-items-center h-100">
+              <img style="width: 95%" src="./assets/img/frenchise.svg" />
+            </div>
+          </div>
+          <div class="col-12 col-lg-6">
+            <div class="card">
+              <div class="card-body py-4">
+                <div class="row">
+                  <div class="col-12">
+                    <form id="desktopContactForm">
+                      <div class="row">
+                        <div class="col-12">
+                          <div class="form-group">
+                            <input type="text" class="form-control" name="name" id="name" aria-describedby="emailHelp"
+                              placeholder="Full Name">
+                            <label class="animate_label" for="exampleInputName1">FULL NAME</label>
+                            <div>
+                              <span class="prettyprint" id="name_error"></span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                          <div class="form-group">
+                            <input type="text" class="form-control" name="contact" id="contact" aria-describedby="emailHelp" maxlength="10"
+                              placeholder="CONTACT NUMBER">
+                            <label class="animate_label" for="exampleInputNumber1">CONTACT NUMBER</label>
+                            <div>
+                              <span class="prettyprint" id="contact_error"></span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                          <div class="form-group">
+                            <input type="email" class="form-control" name="email" id="email"
+                              aria-describedby="emailHelp"
+                              placeholder="Email">
+                            <label class="animate_label" for="exampleInputComment1">EMAIL</label>
+                            <div>
+                              <span class="prettyprint" id="email_error"></span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                          <div class="form-group">
+                            <input type="text" class="form-control" name="pincode" id="pincode" maxlength="6"
+                              aria-describedby="emailHelp" placeholder="Pin Code">
+                            <label class="animate_label" for="exampleInputNumber1">PIN CODE</label>
+                            <div>
+                              <span class="prettyprint" id="pincode_error"></span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                          <div class="form-group">
+                            <div class="select-wrapper">
+                              <select class="form-control" name="area" id="area" aria-describedby="emailHelp">
+                                <option value="" disabled selected>Please Select</option>
+                                <option value="Upto 500 Sqft">Upto 500 Sqft</option>
+                                <option value="Upto 1000 Sqft">Upto 1000 Sqft</option>
+                                <option value="Upto 2000 Sqft">Upto 2000 Sqft</option>
+                                <option value="Upto 5000 Sqft">Upto 5000 Sqft</option>
+                              </select>
+                              <label class="animate_label" for="area">Area in sqft.</label>
+                              <span class="prettyprint" id="area_error"></span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-12 col-md-12">
+                          <div class="form-group">
+                            <select class="form-control" name="propType" id="propType" aria-describedby="emailHelp">
+                              <option value="" selected disabled>Please Select</option>
+                              <option value="Owned">Owned</option>
+                              <option value="Rented">Rented</option>
+                            </select>
+                            <label class="animate_label" for="exampleInputEmail1">Property Type</label>
+                            <div>
+                              <span class="prettyprint" id="propType_error"></span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-12 col-md-12">
+                          <div class="form-group">
+                            <select class="form-control" name="plan" id="plan" aria-describedby="emailHelp">
+                              <option value="" selected disabled>Please Select</option>
+                              <option value="This Month">This Month</option>
+                              <option value="Next Month">Next Month</option>
+                              <option value="After 2 Months">After 2 Months</option>
+                            </select>
+                            <label class="animate_label" for="exampleInputEmail1">When you are planning to
+                              open your
+                              store?</label>
+                            <div>
+                              <span class="prettyprint" id="plan_error"></span>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="ml-3 mb-3">
+                          <span class="prettyprint" id="thank_you_msg_desktop"></span>
+                        </div>
+                        <div class="col-12">
+                          <div class="float-right">
+                            <div class="btn-box border-black">
+                              <button type="submit" class="btn btn-block btn-primary" id="submitBtn"><span>Request
+                                  Callback</span></button>
+                            </div>
+                          </div>
+                        </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="reason_start why_us_section mt-5">
+      <div class="container">
+        <div class="subheading">
+          <h2 class="text-left">Why Partner <span class="main-green">with us?</span></h2>
+        </div>
+        <div class="feat_right_section">
+          <div class="row">
+            <div class="col-12 col-md-4 mb-sm-3">
+              <div class="feat_card">
+                <img src="assets/img/Maximum_Profits.svg" alt="Affordable Pricing">
+                <h3>Maximum Profits</h3>
+                <p>With proven BigDeal franchise model, maximize your profits.</p>
+              </div>
+            </div>
+            <div class="col-12 col-md-4 mb-sm-3">
+              <div class="feat_card">
+                <img src="assets/img/Renowned_Brand.svg" alt="Timely Delivered">
+                <h3>Renowned Brand</h3>
+                <p>Kick-off easily backed by Bigdeal's existing customers.</p>
+              </div>
+            </div>
+            <div class="col-12 col-md-4 mb-sm-3">
+              <div class="feat_card">
+                <img src="assets/img/full_assistance.svg" alt="Experienced Team">
+                <h3>Full Assistance</h3>
+                <p>We're in support throughout.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          <div class="subheading">
+            <h2 class="text-left">Simple <span class="main-green">Procedure</span></h2>
+          </div>
+          <div class="timeline-wrapper">
+            <section class="main-timeline-section mt-5">
+              <div class="timeline-start"></div>
+              <div class="conference-center-line"></div>
+              <div class="conference-timeline-content">
+                <div class="timeline-article timeline-article-top">
+                  <div class="content-date">
+                    <span>1</span>
+                  </div>
+                  <div class="meta-date"></div>
+                  <div class="content-box">
+                    <p>Get in touch with us to obtain the preliminary information.</p>
+                  </div>
+                </div>
+                <div class="timeline-article timeline-article-bottom">
+                  <div class="content-date">
+                    <span>2</span>
+                  </div>
+                  <div class="meta-date"></div>
+                  <div class="content-box">
+                    <p>Talk about objectives, capital and the type of BigDeal franchise.</p>
+                  </div>
+                </div>
+                <div class="timeline-article timeline-article-top">
+                  <div class="content-date">
+                    <span>3</span>
+                  </div>
+                  <div class="meta-date"></div>
+                  <div class="content-box">
+                    <p>Fill up and file the BigDeal franchise application form.</p>
+                  </div>
+                </div>
+                <div class="timeline-article timeline-article-bottom">
+                  <div class="content-date">
+                    <span>4</span>
+                  </div>
+                  <div class="meta-date"></div>
+                  <div class="content-box">
+                    <p>Give out documents and signatures for the agreement where required.</p>
+                  </div>
+                </div>
+                <div class="timeline-article timeline-article-top">
+                  <div class="content-date">
+                    <span>5</span>
+                  </div>
+                  <div class="meta-date"></div>
+                  <div class="content-box">
+                    <p>Select and conclude a profitable location with our help.</p>
+                  </div>
+                </div>
+                <div class="timeline-article timeline-article-bottom">
+                  <div class="content-date">
+                    <span>6</span>
+                  </div>
+                  <div class="meta-date"></div>
+                  <div class="content-box">
+                    <p>Attend training and installation before opening a new Bigdeal franchise!</p>
+                  </div>
+                </div>
+              </div>
+              <div class="timeline-end"></div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mt-5">
+      <?php include './common/free-demo.php'; ?>
+    </div>
+  </article>
+
+  <!-- Blog Section HTML -->
+  <?php include 'common/articles.php'; ?>
+  <!-- Blog Section HTML -->
+
+  <!-- Footer HTML -->
+  <?php include 'common/footer.php'; ?>
+  <!-- Footer HTML -->
+
+  <!-- Floating Mobile Footer -->
+  <?php include 'common/mobile-footer.php'; ?>
+  <!-- Floating Mobile Footer -->
+
+  <!-- Scripts -->
+  <?php include 'common/scripts.php'; ?>
+  <!-- Scripts -->
+  <script>
+    
+</script>
+<script src="assets/js/sheetform.js"></script>
+</body>
+
+</html>
